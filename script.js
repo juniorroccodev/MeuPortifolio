d20d59cecece93e8470a6ab78d85555a3095e42f
Junior Rocco <juniorrocco2020@gmail.com>
2021-06-10 05:34:11 -0300

    novas alterações

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-document.querySelector(".hamburguer").addEventListener("click", () =>
-    document.querySelector(".container").classList.toggle("show-menu")
-);
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass('sticky')
+        }else{
+            $('.navbar').removeClass('sticky')
+        }
+    })
+
+    // menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    })
+})
