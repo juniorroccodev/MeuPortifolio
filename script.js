79d8a70a86f5ab220b0a95dc5d4eb0ec82eb4ced
Junior Rocco <juniorrocco2020@gmail.com>
2021-06-16 04:10:04 -0300

    Atualização

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
-            $('.navbar').addClass('sticky')
-        }else{
-            $('.navbar').removeClass('sticky')
-        }
-    })
+class Accordion{
+    constructor(accordionListQuestion) {
+        this.addcordionListQuestion = document.querySelectorAll(accordionListQuestion);
+        this.activeItemClass = "active";
+    }
 
-    // menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    })
-})
+    toggleAccordion(item) {
+        item.classList.toggle();
+        item.nextElementSibling.classList.toggle(this.activeItemClass);
+    }
+
+    addAccordionEvent() {
+        this.accordionListQuestion.forEach((question) => {
+            question.addEventListener("click", () => this.toggleAccordion(question));
+        });
+    }
+
+    init() {
+        if(this.addcordionListQuestion.length) {
+            this.addAccordionEvent();
+        }
+
+        return this;
+    }
+
+}
+
+const accordion = new Accordion(".fraq-question");
+accordion.init;
